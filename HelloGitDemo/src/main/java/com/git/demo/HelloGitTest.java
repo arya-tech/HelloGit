e0665f04ae55643d4a1b33e089d75e92e4643830
Arya <aryadewan123@gmail.com>
2021-02-08 08:22:18 +0530

    adding friends name in string array

---
+++ HelloGitDemo/src/main/java/com/git/demo/HelloGitTest.java
@@ -0,0 +1,15 @@
+package com.git.demo;
+
+public class HelloGitTest {
+	
+	public static void main(String[] args) {
+		// adding friends name.
+		
+		String[] fName= {"ashok,pradip vikas, mangal"};
+		for(int i=0;i<=fName.length;i++) {
+			System.out.println(fName[i]);
+		}
+		
+	}
+
+}
