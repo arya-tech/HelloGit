6e547a753e558c1ea17762c01f271fab92fbe4e7
Arya <aryadewan123@gmail.com>
2021-02-08 08:38:12 +0530

    adding for each loop instead of old for loop.

--- HelloGitDemo/src/main/java/com/git/demo/HelloGitTest.java
+++ HelloGitDemo/src/main/java/com/git/demo/HelloGitTest.java
@@ -4,12 +4,11 @@ public class HelloGitTest {
 	
 	public static void main(String[] args) {
 		// adding friends name.
-		
+		//changing the for loop
 		String[] fName= {"ashok,pradip vikas, mangal"};
 		for(int i=0;i<=fName.length;i++) {
 			System.out.println(fName[i]);
 		}
-		
 	}
 
 }
